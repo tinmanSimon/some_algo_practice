bdb8e740c658816f1afefa23f79fffe6dff557d4
simon <wudisas@gmail>
2020-08-19 19:46:47 -0400

    Added bool evaluation

--- learn_python.py
+++ learn_python.py
@@ -444,5 +444,33 @@ def n_queens(n, board_size = 8):
     helper(0,n)
     return queen_record
 
-print(n_queens(8))
+#returns number of ways to parenthesizing the s and produce the result
+def bool_eval(s, r):
+    result = ""
+    if r == False: result = "0"
+    else: result = "1"
+    def helper(s, result, startIndex, endIndex):
+        if startIndex >= endIndex: raise ValueError("Error - bool_eval::helper - msg: startIndex >= endIndex")
+        if startIndex + 1 == endIndex: 
+            if s[startIndex] == result: return 1
+            else: return 0
+        ret = 0
+        for i in range(startIndex + 1, endIndex, 2):
+            leftTrue = helper(s, "1", startIndex, i)
+            rightTrue = helper(s, "1", i + 1, endIndex)
+            leftFalse = helper(s, "0", startIndex, i)
+            rightFalse = helper(s, "0", i + 1, endIndex)
+            if s[i] == "&": 
+                if result == "1": ret += leftTrue * rightTrue
+                else: ret += leftTrue * rightFalse + leftFalse * rightTrue + leftFalse * rightFalse
+            elif s[i] == "|": 
+                if result == "1": ret += leftTrue * rightTrue + leftTrue * rightFalse + leftFalse * rightTrue
+                else: ret += leftFalse * rightFalse
+            elif s[i] == "^": 
+                if result == "1": ret += leftTrue * rightFalse + leftFalse * rightTrue
+                else: ret += leftTrue * rightTrue + leftFalse * rightFalse
+        return ret
+    
+    return helper(s, result, 0, len(s))
+
 
